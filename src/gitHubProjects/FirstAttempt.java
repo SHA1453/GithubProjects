638087dab789b4ee964b2390fb5855e75da910bc
Hamza <seyyidalcik@gmail.com>
2022-12-15 19:35:32 +0100

    First Action

---
+++ src/gitHubProjects/FirstAttempt.java
@@ -0,0 +1,10 @@
+package gitHubProjects;
+
+public class FirstAttempt {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub, My name is Hamza");
+        System.out.println("Merhaba Github, Benim adim Hamza");
+        System.out.println("Bonjour Github, Je m'appel Hamza");
+    }
+
+}
